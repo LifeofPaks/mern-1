0652445f784a9cc60ec5a512ae5d2f18c47ce5ca
lifeofpaks <promise@alquimiavc.com>
2024-10-21 15:45:13 +0100

    updated vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000', // your backend server
+        target: 'http://localhost:4000', 
         changeOrigin: true,
         secure: false,
       },
