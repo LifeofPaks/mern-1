aa682b602fc5cce5ce488f04c333840c0cae8ea2
lifeofpaks <promise@alquimiavc.com>
2024-10-24 09:24:53 +0100

    created login and signup page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,16 @@ import "./index.css";
 
 import { BrowserRouter as Router } from "react-router-dom";
 import { WorkoutContextProvider } from "./context/WorkoutContext.jsx";
+import { AuthContextProvider } from "./context/AuthContext.jsx";
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Router>
-      <WorkoutContextProvider>
-        <App />
-      </WorkoutContextProvider>
+      <AuthContextProvider>
+        <WorkoutContextProvider>
+          <App />
+        </WorkoutContextProvider>
+      </AuthContextProvider>
     </Router>
   </StrictMode>
 );
